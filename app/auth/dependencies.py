57100ad9348ad1ed6079aa26cc6d773e1058122e
SoaresBryan <161089460+SoaresBryan@users.noreply.github.com>
2024-11-06 11:41:53 -0300

    troca de lib python-jose para pyjwt

--- app/auth/dependencies.py
+++ app/auth/dependencies.py
@@ -1,6 +1,7 @@
 from fastapi import HTTPException, Depends, status
 from fastapi.security import OAuth2PasswordBearer
-from jose import JWTError, jwt
+import jwt
+from jwt import PyJWTError
 from app.config import SECRET_KEY, ALGORITHM
 from app.db.utils_db import find_user_by_username
 
@@ -8,11 +9,12 @@ oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
 
 async def get_current_user(token: str = Depends(oauth2_scheme)):
     try:
+        # Decodifica o token JWT usando pyjwt
         payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
         username: str = payload.get("sub")
         if username is None:
             raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Usuário não encontrado no token")
-    except JWTError:
+    except PyJWTError:
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Erro ao decodificar o JWT")
 
     user = await find_user_by_username(username)
--- app/auth/utils_auth.py
+++ app/auth/utils_auth.py
@@ -1,9 +1,13 @@
 from datetime import datetime, timedelta
-from jose import jwt
+import jwt 
 from app.config import SECRET_KEY, ALGORITHM, ACCESS_TOKEN_EXPIRE_DELTA
 
 def create_access_token(data: dict):
+    """Cria um token de acesso JWT usando pyjwt."""
     to_encode = data.copy()
     expire = datetime.utcnow() + ACCESS_TOKEN_EXPIRE_DELTA
     to_encode.update({"exp": expire})
-    return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    
+    # Gera o token JWT usando pyjwt
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
